46a8d039b30713334560da881520119b43db90c1
Magali-Cristobo <alumno21.cristobo.magali@ipm.edu.ar>
2021-06-25 12:16:37 -0300

    soluciono error del boton del login

--- login.php
+++ login.php
@@ -3,10 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script
-      src="https://kit.fontawesome.com/64d58efce2.js"
-      crossorigin="anonymous"
-    ></script>
+    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="style.css" />
     <title>Registro / Inicio de sesion</title>
   </head>
@@ -15,7 +12,7 @@
     <div class="container">
       <div class="forms-container">
         <div class="signin-signup">
-          <form action="#" class="sign-in-form">
+          <form class="sign-in-form" onsubmit="return irAInicio(event)">
             <h2 class="title">Iniciar sesion</h2>
             <div class="input-field">
               <i class="fas fa-user"></i>
@@ -46,13 +43,14 @@
       </div>
     </div>
     <script>
-      function irAInicio(){
-        if(document.getElementById("usuario").value!="" && document.getElementById("password").value!=""){
-            console.log("entre");
+      function irAInicio(e){
+          e.preventDefault();
+          if(document.getElementById("usuario").value!="" && document.getElementById("password").value!=""){
           location.href="index.php";
         }
       }
-      $(".btn").click(irAInicio);
+      $("button").click(irAInicio);
+
     </script>
   </body>
 </html>
